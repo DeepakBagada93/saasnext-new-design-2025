f4ea12baf0a5f46c945033ea11cf826a7beab9ac
Deepak Bagada <deeepakbagada25@gmail.com>
2025-10-11 06:22:46 +0000

    Try fixing this error: `Console Error: params are being enumerated. `par

--- src/app/client/invoices/[id]/page.tsx
+++ src/app/client/invoices/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import QuotationDetails from './invoice-details';
 
-export default function QuotationPage({ params: { id } }: { params: { id: string } }) {
-    return <QuotationDetails quotationId={id} />;
+export default function QuotationPage({ params }: { params: { id: string } }) {
+    return <QuotationDetails quotationId={params.id} />;
 }
